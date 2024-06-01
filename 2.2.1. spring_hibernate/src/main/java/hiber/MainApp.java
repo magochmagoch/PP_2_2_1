d91e19b4f0edd875887070f356d65f85de50b0f9
Магомед Ахмедов <magomed.ahmedov1993@mail.ru>
2024-06-02 00:43:32 +0300

    Initial commit

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -11,53 +11,52 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class MainApp {
-   public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
-            new AnnotationConfigApplicationContext(AppConfig.class);
+    public static void main(String[] args) throws SQLException {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
-      UserService userService = context.getBean(UserService.class);
-      CarService carService = context.getBean(CarService.class);
+        UserService userService = context.getBean(UserService.class);
+        CarService carService = context.getBean(CarService.class);
 
-      User user1 = new User("User1", "Lastname1", "user1@mail.ru");
-      User user2 = new User("User2", "Lastname2", "user2@mail.ru");
-      User user3 = new User("User3", "Lastname3", "user3@mail.ru");
-      User user4 = new User("User4", "Lastname4", "user4@mail.ru");
+        User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+        User user2 = new User("User2", "Lastname2", "user2@mail.ru");
+        User user3 = new User("User3", "Lastname3", "user3@mail.ru");
+        User user4 = new User("User4", "Lastname4", "user4@mail.ru");
 
-      Car car1 = new Car("Prada Liora", 112233);
-      Car car2 = new Car("Coyota Tamry", 223344);
-      Car car3 = new Car("Must Fordang", 334455);
-      Car car4 = new Car("Ria Kio", 556677);
+        Car car1 = new Car("Prada Liora", 112233);
+        Car car2 = new Car("Coyota Tamry", 223344);
+        Car car3 = new Car("Must Fordang", 334455);
+        Car car4 = new Car("Ria Kio", 556677);
 
-      user1.setCar(car1);
-      user2.setCar(car2);
-      user3.setCar(car3);
-      user4.setCar(car4);
+        user1.setCar(car1);
+        user2.setCar(car2);
+        user3.setCar(car3);
+        user4.setCar(car4);
 
-      carService.add(car1);
-      carService.add(car2);
-      carService.add(car3);
-      carService.add(car4);
+        carService.add(car1);
+        carService.add(car2);
+        carService.add(car3);
+        carService.add(car4);
 
-      userService.add(user1);
-      userService.add(user2);
-      userService.add(user3);
-      userService.add(user4);
+        userService.add(user1);
+        userService.add(user2);
+        userService.add(user3);
+        userService.add(user4);
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println("Car Id = "+user.getCar().getId());
-         System.out.println("Car Model = "+user.getCar().getModel());
-         System.out.println("Car Series = "+user.getCar().getSeries());
-         System.out.println();
-      }
+        List<User> users = userService.listUsers();
+        for (User user : users) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            System.out.println("Car Id = " + user.getCar().getId());
+            System.out.println("Car Model = " + user.getCar().getModel());
+            System.out.println("Car Series = " + user.getCar().getSeries());
+            System.out.println();
+        }
 
-      System.out.println(userService.getUserByCar("Ria Kio", 112233));
-      System.out.println(userService.getUserByCar("Coyota Tamry", 223344).getFirstName());
+        System.out.println(userService.getUserByCar("Ria Kio", 112233));
+        System.out.println(userService.getUserByCar("Coyota Tamry", 223344).getFirstName());
 
-      context.close();
-   }
+        context.close();
+    }
 }
